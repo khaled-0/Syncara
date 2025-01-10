af80548babf31d402ce8d62321e9d2eaf167d5dc
PranavPurwar <purwarpranav80@gmail.com>
2025-01-10 15:51:21 +0530

    docs: Improve website performance and seo

--- docs/src/main.ts
+++ docs/src/main.ts
@@ -1,7 +1,14 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import VueLazyLoad from 'vue3-lazyload'
 import App from './App.vue'
 
 createApp(App)
+.use(VueLazyLoad, {
+  observerOptions: {
+    rootMargin: '0px',
+    threshold: 0.1
+  }
+})
 .mount('#app')
