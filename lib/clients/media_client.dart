429f9bf0e644b85dbc7018fc21f3c6c64dcdff22
Khaled <spkhalad@gmail.com>
2025-12-31 23:49:14 +0600

    fix: Compatibility with older downloaded files [skip-ci]

--- lib/clients/media_client.dart
+++ lib/clients/media_client.dart
@@ -42,7 +42,7 @@ class MediaClient implements BaseMediaClient {
 
   File mediaFile(Media media) {
     return File(
-      media.localPath ?? path.join(downloadsDir, path.basename(media.url)),
+      media.localPath ?? path.join(downloadsDir, path.basename(media.url).split("=").last),
     );
   }
 
