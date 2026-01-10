92a7d2954757a2c4dd277199ea541f3603d1effd
Khaled <spkhalad@gmail.com>
2026-01-10 18:54:50 +0600

    chore: Add test,markdown_plus dependencies
    Add debug suffix for android

--- lib/clients/in_app_update_client.dart
+++ lib/clients/in_app_update_client.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:http/http.dart' as http;
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:url_launcher/url_launcher_string.dart';
